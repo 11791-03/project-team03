ca4924d35e4e6f49244f10b2b738f75cbf1283f2
EDFward <edfward@gmail.com>
2014-11-10 21:26:50 -0500

    finished doc retrieval skeleton

--- src/main/java/edu/cmu/lti/f14/project/DocumentRetrieval.java
+++ src/main/java/edu/cmu/lti/f14/project/DocumentRetrieval.java
@@ -1,27 +1,41 @@
 package edu.cmu.lti.f14.project;
 
+import edu.cmu.lti.oaqa.bio.bioasq.services.GoPubMedService;
+import edu.cmu.lti.oaqa.bio.bioasq.services.PubMedSearchServiceResponse;
+import edu.cmu.lti.oaqa.type.input.Question;
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.FSIterator;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import util.TypeFactory;
 
-import edu.cmu.lti.oaqa.type.input.*;
+import java.io.IOException;
 
 /**
  * @author junjiah
- *
  */
 public class DocumentRetrieval extends JCasAnnotator_ImplBase {
+
+  private GoPubMedService service;
+
+  private static final String URI_PREFIX = "http://www.ncbi.nlm.nih.gov/pubmed/";
 
   /**
    *
    */
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
-  };
+    try {
+      service = new GoPubMedService("project.properties");
+    } catch (ConfigurationException e) {
+      System.err.println("ERROR: Initialize PubMed service error in Document Retrieval.");
+      System.exit(1);
+    }
+  }
 
   /**
    *
@@ -30,8 +44,25 @@ public class DocumentRetrieval extends JCasAnnotator_ImplBase {
   public void process(JCas aJCas) throws AnalysisEngineProcessException {
     for (FeatureStructure featureStructure : aJCas.getAnnotationIndex(Question.type)) {
       Question question = (Question) featureStructure;
-      // do something here to extract the concepts
-    }
+      String query = question.getPreprocessedText();
+      if (query == null || query.isEmpty())
+        return;
 
+      PubMedSearchServiceResponse.Result pubMedResult = null;
+      System.out.println(query);
+      try {
+        pubMedResult = service.findPubMedCitations(query, 0);
+      } catch (IOException e) {
+        System.err.println("ERROR: Search PubMed in Document Retrieval Failed.");
+        System.exit(1);
+      }
+
+      for (PubMedSearchServiceResponse.Document pubMedDocument : pubMedResult.getDocuments()) {
+        String pmid = pubMedDocument.getPmid();
+        Document document = TypeFactory
+                .createDocument(aJCas, URI_PREFIX + pmid, pubMedDocument.getPmid());
+        document.addToIndexes();
+      }
+    }
   }
 }
--- src/main/java/edu/cmu/lti/f14/project/QuestionPreprocessing.java
+++ src/main/java/edu/cmu/lti/f14/project/QuestionPreprocessing.java
@@ -1,60 +1,90 @@
 package edu.cmu.lti.f14.project;
 
-import java.io.IOException;
-
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.http.client.ClientProtocolException;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import edu.cmu.lti.oaqa.type.input.Question;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.FeatureStructure;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import util.StanfordLemmatizer;
 
-import edu.cmu.lti.oaqa.bio.bioasq.services.GoPubMedService;
-import edu.cmu.lti.oaqa.bio.bioasq.services.OntologyServiceResponse;
-import edu.cmu.lti.oaqa.type.input.Question;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 /**
- * @author junjiah
+ * Process the original question texts with tokenization, stemming and stop-word filtering.
  *
+ * @author junjiah
  */
 public class QuestionPreprocessing extends JCasAnnotator_ImplBase {
 
   /**
-   *
+   * Set up a stop-word dictionary.
    */
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
-  };
+    super.initialize(aContext);
+    stopwords = Sets.newHashSet();
+
+    // read stop-words from file
+    InputStream stopwordsStream = getClass().getResourceAsStream("/stopwords.txt");
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new InputStreamReader(stopwordsStream));
+      String line;
+      while ((line = br.readLine()) != null) {
+        stopwords.add(line);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (br != null) {
+          br.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
   /**
-   *
+   * Simply add tokenized text to the CAS.
    */
   @Override
   public void process(JCas aJCas) throws AnalysisEngineProcessException {
     for (FeatureStructure featureStructure : aJCas.getAnnotationIndex(Question.type)) {
       Question question = (Question) featureStructure;
-      GoPubMedService service;
-      try {
-        service = new GoPubMedService("project.properties");
-        OntologyServiceResponse.Result diseaseOntologyResult = service
-                .findDiseaseOntologyEntitiesPaged(question.getText(), 0);
-        System.out.println("Disease ontology: " + diseaseOntologyResult.getFindings().size());
-        for (OntologyServiceResponse.Finding finding : diseaseOntologyResult.getFindings()) {
-          
-          System.out.println(" > " + finding.getConcept().getLabel() + " "
-                  + finding.getConcept().getUri());
-        }
-      } catch (ConfigurationException e) {
-        e.printStackTrace();
-      } catch (ClientProtocolException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
+      String text = question.getText();
+      List<String> tokenizedText = tokenize0(text);
+      question.setPreprocessedText(String.join(" ", tokenizedText));
     }
-
   }
+
+  /**
+   * A simple tokenizer doing stemming and stop-word filtering.
+   *
+   * @param text Original text
+   * @return Tokenized text
+   */
+  private List<String> tokenize0(String text) {
+    List<String> res = Lists.newArrayList();
+    StringTokenizer tokenizer = new StringTokenizer(text, " .,?!:;()<>[]\b\t\n\f\r\"\'\"");
+    while (tokenizer.hasMoreElements()) {
+      String token = tokenizer.nextToken().trim();
+      if (!stopwords.contains(token))
+        res.add(StanfordLemmatizer.stemWord(token));
+    }
+    return res;
+  }
+
+  private Set<String> stopwords;
 }
