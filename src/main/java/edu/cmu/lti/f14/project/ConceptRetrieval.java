161eb6547e15503a81034f0acda71e5f5c0cbd1c
EDFward <edfward@gmail.com>
2014-11-25 18:42:50 -0500

    concept silence! added NER model

--- src/main/java/edu/cmu/lti/f14/project/ConceptRetrieval.java
+++ src/main/java/edu/cmu/lti/f14/project/ConceptRetrieval.java
@@ -98,15 +98,15 @@ public class ConceptRetrieval extends JCasAnnotator_ImplBase {
   }
 
   private void createConcept(JCas jcas, Concept c, double score) {
-    System.out.println(">" + c.getLabel());
+//    System.out.println(">" + c.getLabel());
 //    System.out.println("-->\t\t\t" + c.getUri());
     // score 0.1 - Concept - precision: 0.0178, recall: 0.1357, F1: 0.0314, MAP: 0.0655, GMAP:
     // 0.0311
     // score 0.15 - Concept - precision: 0.0364, recall: 0.0929, F1: 0.0523, MAP: 0.0841, GMAP:
     // 0.0321
-    System.out.println(score);
+//    System.out.println(score);
     if (score > 0.15) {
-      System.out.println("selected:\t" + score);
+//      System.out.println("selected:\t" + score);
       if (validConcept(jcas, c.getLabel().toLowerCase())) {
         conceptsSoFar.add(c.getLabel().toLowerCase());
         TypeFactory.createConcept(jcas, c.getUri().replace("2014", "2012")).addToIndexes();
