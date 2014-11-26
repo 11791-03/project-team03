7d44b70f25baea2525d985aed9dc5ff4622e9594
Chu-Cheng Lin <chuchenglin@cmu.edu>
2014-11-25 20:36:04 -0500

    extract NEs from questions

--- src/main/java/edu/cmu/lti/f14/project/util/NEChunker.java
+++ src/main/java/edu/cmu/lti/f14/project/util/NEChunker.java
@@ -1,21 +1,30 @@
 package edu.cmu.lti.f14.project.util;
 
-import com.aliasi.chunk.Chunk;
-import com.aliasi.chunk.Chunker;
-import com.aliasi.chunk.Chunking;
-import com.aliasi.util.AbstractExternalizable;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.Chunker;
+import com.aliasi.chunk.Chunking;
+import com.aliasi.util.AbstractExternalizable;
+
 public class NEChunker {
   final static String modelPath = "/ne-en-bio-genetag.HmmChunker";
 
-  Chunker ch;
+  Chunker ch = null;
 
-  public NEChunker() {
+  private static NEChunker neChunker = null;
+
+  public static NEChunker getInstance() {
+    if (neChunker == null) {
+      neChunker = new NEChunker();
+    }
+    return neChunker;
+  }
+
+  private NEChunker() {
     try {
       ch = (Chunker) AbstractExternalizable.readResourceObject(NEChunker.class, modelPath);
     } catch (ClassNotFoundException | IOException e) {
